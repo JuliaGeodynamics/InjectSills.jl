06ee37578b576a4022f4a984d678c2e78f3aa252
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2026-04-18 10:25:42 +0200

    Update example_JustPIC.jl

--- examples/example_JustPIC.jl
+++ examples/example_JustPIC.jl
@@ -61,8 +61,8 @@ function main()
 
     particles = init_particles(backend, nxcell, max_xcell, min_xcell, xvi...)
 
-    #sill2D = PennyShapedSill(Center=Point2(0, -5000)*m, H=40.0m, W=2000.0m, Angle=Vec1(30))
-    sill2D = McTigueSphere(Center=Point2(0, -5000)*m, r=15.0m)
+    sill2D = PennyShapedSill(Center=Point2(0, -5000)*m, H=40.0m, W=2000.0m, Angle=Vec1(30))
+    #sill2D = McTigueSphere(Center=Point2(0, -5000)*m, r=15.0m)
 
     Dx, Dy = init_cell_arrays(particles, Val(2))
     inject_sill!(particles, Dx, Dy, xvi, sill2D)
